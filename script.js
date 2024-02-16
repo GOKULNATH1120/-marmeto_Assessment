7ab9141e6b089a437701a9d5a93ef681b9047fa0
GOKULNATH1120 <gokulnath0321@gmail.com>
2024-02-16 09:59:07 +0530

    button style fixed

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let quantity = 1;
 let selectedColor = '';
 let selectedSize = '';
 
+
+// Image update function
 function updateMainImage(selectedImage) {
     var mainImage = document.getElementById('mainImage');
     mainImage.src = selectedImage.src;
@@ -9,6 +11,8 @@ function updateMainImage(selectedImage) {
     mainImage.style.height = 'auto';
 }
 
+
+// Inc & Dec buttons
 function decrementQuantity() {
     if (quantity > 1) {
         quantity--;
@@ -21,6 +25,8 @@ function incrementQuantity() {
     document.getElementById('quantity').innerText = quantity;
 }
 
+
+// Add cart Button
 function addToCart() {
     if (selectedColor && selectedSize) {
         const cart = document.getElementById('cart');
@@ -29,8 +35,8 @@ function addToCart() {
         item.style.backgroundColor = '#E7F8B7';
         item.style.fontWeight = "600"
         item.style.color = '#000000';
-        item.style.padding = '5px '; // Add padding
-        item.style.borderRadius = '8px'; // Add rounded corners
+        item.style.padding = '5px '; 
+        item.style.borderRadius = '8px'; 
         cart.appendChild(item);
     } else {
         alert('Please select color and size');
@@ -49,6 +55,8 @@ document.querySelectorAll('input[type="radio"][name="size"]').forEach(input => {
     });
 });
 
+
+// Color box 
 
 let selectedColorBox = null;
 
